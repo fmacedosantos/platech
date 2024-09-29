d83a9e46751274c1cf13d2c00dc1264d4db28eb6
fmacedosantos <felipemacedo986@gmail.com>
2024-09-29 16:14:18 -0300

    Linkando o cadastro-cliente no menu (solução temporária)

--- docs/src/pagina-inicial/script.js
+++ docs/src/pagina-inicial/script.js
@@ -5,3 +5,13 @@ document.querySelector(".menu-toggle").addEventListener("click", function () {
 document.querySelector(".overlay").addEventListener("click", function () {
   document.body.classList.remove("navbar-open");
 });
+
+const clientMenu = document.getElementById('clientMenu');
+
+clientMenu.addEventListener('change', function() {
+  const selectedOption = this.value; 
+
+  if (selectedOption == 'Cadastrar Clientes') { 
+    window.location.href = '../cadastro-cliente/cadastro-cliente.html'; 
+  }
+});
