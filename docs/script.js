aaea856a7d6c126bacd319586c70f70f6cc67c83
fmacedosantos <felipemacedo986@gmail.com>
2024-09-29 20:46:33 -0300

    Criando a máscara de cpf com js puro

--- docs/script.js
+++ docs/script.js
@@ -5,3 +5,15 @@ getElement.addEventListener("submit", function (event) {
 
   window.location.href = "src/pagina-inicial/pagina-inicial.html";
 });
+
+const cpf = document.getElementById('cpf');
+
+cpf.addEventListener('keypress', () => {
+  let cpfLength = cpf.value.length;
+
+  if (cpfLength === 3 || cpfLength ===7) {
+    cpf.value += '.';
+  } else if (cpfLength === 11) {
+    cpf.value += '-';
+  }
+})
