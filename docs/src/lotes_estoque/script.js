0a46d8b025730529d9fdf02920ddb20b9eb80d9c
Felipe Macedo dos Santos <felipemacedo986@gmail.com>
2024-09-30 09:33:37 -0300

    Adicionando o menu provisoriamente

--- docs/src/lotes_estoque/script.js
+++ docs/src/lotes_estoque/script.js
@@ -13,3 +13,42 @@ optionsList.forEach(option => {
         selectBox.classList.remove('active');
     });
 });
+
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+    document.body.classList.toggle("navbar-open");
+  });
+  
+  document.querySelector(".overlay").addEventListener("click", function () {
+    document.body.classList.remove("navbar-open");
+  });
+  
+  const clientMenu = document.getElementById('clientMenu');
+  
+  clientMenu.addEventListener('change', function() {
+    const selectedOption = this.value; 
+  
+    if (selectedOption == 'Cadastrar Clientes') { 
+      window.location.href = '../cadastro-cliente/cadastro-cliente.html'; 
+    }
+  
+    if (selectedOption == 'Fechamento') {
+      window.location.href = '../fechamento-clientes/fechamento-cliente.html'; 
+    }
+  });
+  
+  const controlMenu = document.getElementById('controlMenu');
+  
+  controlMenu.addEventListener('change', function() {
+    const selectedOption = this.value;
+  
+    if (selectedOption == 'Lotes') {
+      window.location.href= '../lotes_estoque/index.html';
+    }
+  })
+  
+
+  const homeButton = document.getElementById('homeButton');
+
+homeButton.addEventListener('click', () => {
+  window.location.href = '../pagina-inicial/pagina-inicial.html'
+})
