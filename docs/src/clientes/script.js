71204f19c295c6689b6a01dc6e13079e50b2cca1
ViniciusJacomo <viniciusmatosjacomo@gmail.com>
2024-11-03 11:40:46 -0300

    commit Tela Todos clientes

---
+++ docs/src/clientes/script.js
@@ -0,0 +1,49 @@
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.body.classList.toggle("navbar-open");
+});
+
+document.querySelector(".overlay").addEventListener("click", function () {
+  document.body.classList.remove("navbar-open");
+});
+
+const clientMenu = document.getElementById("clientMenu");
+
+clientMenu.addEventListener("change", function () {
+  const selectedOption = this.value;
+
+ 
+  if (selectedOption == "Cadastrar Clientes") {
+    window.location.href = "../cadastro-cliente/cadastro-cliente.html";
+  } else if (selectedOption == "Fechamento") {
+    window.location.href = "../fechamento-clientes/fechamento-cliente.html";
+  } else if (selectedOption == "Clientes") {
+    window.location.href = "../clientes/index.html"; 
+  }
+});
+
+
+const homeButton = document.getElementById("homeButton");
+
+homeButton.addEventListener("click", () => {
+  window.location.href = "../pagina-inicial/pagina-inicial.html";
+});
+
+const controlMenu = document.getElementById("controlMenu");
+
+controlMenu.addEventListener("change", function () {
+  const selectedOption = this.value;
+
+  if (selectedOption == "Lotes") {
+    window.location.href = "../lotes_estoque/index.html";
+  }
+});
+
+const placMenu = document.getElementById("placMenu");
+
+placMenu.addEventListener("change", function () {
+  const selectedOption = this.value;
+
+  if (selectedOption == "Registrar placas") {
+    window.location.href = "../registrar-placa/registrar-placas.html";
+  }
+});
