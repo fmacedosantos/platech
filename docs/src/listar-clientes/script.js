2fbd570ba287501d20e74e4586677ac84019ed57
Php22 <php__@tutamail.com>
2024-11-03 13:26:09 -0300

    Padroniza o telefone

--- docs/src/listar-clientes/script.js
+++ docs/src/listar-clientes/script.js
@@ -49,3 +49,41 @@ controlMenu.addEventListener("change", function () {
     window.location.href = "../lotes_estoque/index.html";
   }
 });
+
+// Lista os clientes
+async function fetchAndDisplayClients() {
+  try {
+      const response = await fetch('http://localhost/api-platech/clients/fetch');
+      if (!response.ok) {
+          throw new Error('Erro ao buscar dados da API');
+      }
+
+      const data = await response.json();
+      console.log("API Response:", data);
+
+      if (data.success && data.data) {
+          const tableBody = document.getElementById('platesTableBody');
+          tableBody.innerHTML = "";
+
+          data.data.forEach(client => {
+              const row = document.createElement('tr');
+
+              row.innerHTML = `
+                  <td>${client.applicant}</td>
+                  <td>${client.company ? client.company : 'Particular'}</td>
+                  <td>${client.phone}</td>
+              `;
+
+              tableBody.appendChild(row);
+          });
+      } else {
+          alert("Nenhum dado encontrado.");
+      }
+  } catch (error) {
+      console.error("Erro na requisição:", error);
+      alert("Erro ao conectar com a API.");
+  }
+}
+
+
+window.onload = fetchAndDisplayClients;
